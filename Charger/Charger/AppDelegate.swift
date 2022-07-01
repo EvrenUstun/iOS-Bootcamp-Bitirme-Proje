c4b9c0da8632909cbc2c531a08d561c41ebcedfa
Evren Ustun <ustunnevren@gmail.com>
2022-07-01 21:43:29 +0300

    Added launch screen.

---
+++ Charger/Charger/AppDelegate.swift
@@ -0,0 +1,44 @@
+//
+//  AppDelegate.swift
+//  Charger
+//
+//  Created by Evren Ustun on 25.06.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        ProjectRepository.deviceUDID = UIDevice.current.identifierForVendor?.uuidString
+        
+        let attrs = [
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 20.0, weight: .bold)
+        ]
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor(red: 0.26, green: 0.29, blue: 0.33, alpha: 1)
+        appearance.titleTextAttributes = attrs
+        
+        UIBarButtonItem.appearance().setTitleTextAttributes(attrs, for: .normal)
+        return true
+    }
+    
+    // MARK: UISceneSession Lifecycle
+    
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+    
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+}
