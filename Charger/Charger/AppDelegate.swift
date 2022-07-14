dbf366b8027033e618dac012395e35997f2f7792
Evren Ustun <ustunnevren@gmail.com>
2022-07-14 21:01:46 +0300

    Added notification manager.

--- Charger/Charger/AppDelegate.swift
+++ Charger/Charger/AppDelegate.swift
@@ -10,7 +10,20 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
+    private func requestNotificationAuthorization(application: UIApplication){
+        let center = UNUserNotificationCenter.current()
+        let options: UNAuthorizationOptions = [.alert, .badge, .sound]
+        
+        center.requestAuthorization(options: options) { granted, error in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        requestNotificationAuthorization(application: application)
         
         ProjectRepository.deviceUDID = UIDevice.current.identifierForVendor?.uuidString
         
@@ -39,6 +52,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        UIApplication.shared.applicationIconBadgeNumber = 0
     }
     
 }
